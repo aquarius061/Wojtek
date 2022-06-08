cfdebf5987d81cd3d01c317cfa015edf10ce818e
Michal Strzyzewski <michal.strzyzewski@rb-imconsult.com>
2022-06-08 15:22:23 +0200

    Konwersja int->char*

--- SumaCyfr/main.cpp
+++ SumaCyfr/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
 
 using namespace std;
 
@@ -18,11 +21,29 @@ int getSumaCyfr(int liczba)
     return wynik;
 }
 
+int getSumaCyfrString(int liczba)
+{
+    char str[10];
+    char c[2];
+    int len = sprintf(str, "%d", liczba);
+    int num = 0;
+    int wynik = 0;
+    for (int i = 0; i < len; i++)
+    {
+        c[0] = str[i];
+        c[1] = '\0';
+        num = atoi(c);
+        wynik += num;
+    }
+    return wynik;
+}
+
 int main()
 {
     cout << "Podaj liczbe : ";
     int liczba;
     cin >> liczba;
-    cout << "Suma cyfr : " << getSumaCyfr(liczba) << endl;
+    cout << "Suma cyfr dla liczby (bez konswersji na string) " << liczba << " wynosi : " << getSumaCyfr(liczba) << endl;
+    cout << "Suma cyfr dla liczby (z kownersja na string) " << liczba << " wynosi : " << getSumaCyfrString(liczba) << endl;
     return 0;
 }

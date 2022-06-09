571eec2025937dafdad45a622207732c7d68bf6a
Michal Strzyzewski <michal.strzyzewski@rb-imconsult.com>
2022-06-09 11:32:58 +0200

    Lepszy sposób
    Lepszy sposób na oblicznie sumy cyfr oaz liczby cyfr

--- SumaCyfr/main.cpp
+++ SumaCyfr/main.cpp
@@ -8,32 +8,22 @@ using namespace std;
 
 int getSumaCyfr(int liczba)
 {
-    int cyfry = log10(liczba);
-    cout << "Liczba cyfr : " << cyfry << endl;
     int wynik = 0;
-    for (int i=cyfry; i >=0; i--)
+    while (liczba > 0)
     {
-        int a = liczba / rint(pow(10,i));
-        liczba -= rint(pow(10,i)) * a;
-        wynik += a;
-        cout << a << " liczba = " << liczba << endl;
+        wynik+=liczba%10;
+        liczba/=10;
     }
     return wynik;
 }
 
-int getSumaCyfrString(int liczba)
+int getLiczbaCyfr(int liczba)
 {
-    char str[10];
-    char c[2];
-    int len = sprintf(str, "%d", liczba);
-    int num = 0;
     int wynik = 0;
-    for (int i = 0; i < len; i++)
+    while (liczba > 0)
     {
-        c[0] = str[i];
-        c[1] = '\0';
-        num = atoi(c);
-        wynik += num;
+        wynik++;
+        liczba/=10;
     }
     return wynik;
 }
@@ -43,7 +33,7 @@ int main()
     cout << "Podaj liczbe : ";
     int liczba;
     cin >> liczba;
-    cout << "Suma cyfr dla liczby (bez konswersji na string) " << liczba << " wynosi : " << getSumaCyfr(liczba) << endl;
-    cout << "Suma cyfr dla liczby (z kownersja na string) " << liczba << " wynosi : " << getSumaCyfrString(liczba) << endl;
+    cout << "Suma cyfr dla liczby " << liczba << " wynosi : " << getSumaCyfr(liczba) << endl;
+    cout << "Liczba " << liczba << " ma " << getLiczbaCyfr(liczba) << " cyfr(y) " << endl;
     return 0;
 }
